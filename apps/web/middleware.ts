6c088258bc886e3197fd600ea5562ff8a5f44245
Jahanwee . <jahan@MacBook-Air.local>
2026-06-17 02:38:53 +0530

    fix(web): restore default middleware matcher to fix css asset loading

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -19,6 +19,6 @@ export function middleware(req: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|.*\\.css|.*\\.js).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
   ],
 };
